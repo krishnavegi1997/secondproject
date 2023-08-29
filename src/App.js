675ba840df3c6724ebb821c8c326691ab57abd01
saikrishnavegi <saikrishnavegi529@gmail.com>
2023-08-29 19:21:11 +0530

    myproject

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Counter from './Components/Counter/Counter'
-import Todolist from './Components/Todolist/Todolist'
 
 function App() {
   return (
    <div>
     <Counter/>
-    <Todolist></Todolist>
    </div>
   )
 }
