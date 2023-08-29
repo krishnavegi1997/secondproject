07839029a3e83c5d8ea83d7d29d67ae5ed48fe01
saikrishnavegi <saikrishnavegi529@gmail.com>
2023-08-29 18:37:23 +0530

    Todolist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Counter from './Components/Counter/Counter'
+import Todolist from './Components/Todolist/Todolist'
 
 function App() {
   return (
    <div>
     <Counter/>
+    <Todolist/>
    </div>
   )
 }
