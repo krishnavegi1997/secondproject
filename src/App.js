d9d78e2893bc854f713a0d9ed82b67b7d697ae31
saikrishnavegi <saikrishnavegi529@gmail.com>
2023-09-25 10:10:24 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Headers from './Components/headers/Headers'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
   <div>
-
+  <Headers/>
+  <Outlet/>
   </div>
   )
 }
