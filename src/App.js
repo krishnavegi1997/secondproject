34078e6286f7e58349cdd9b7381006a82c22d14f
saikrishnavegi <saikrishnavegi529@gmail.com>
2023-09-26 10:50:52 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Headers from './Components/headers/Headers'
+import { Outlet } from 'react-router-dom'
 function App() {
   return (
   <div>
-hii
+<Headers/>
+<Outlet/>
   </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Login from './Components/login/Login';
+import UserDetails from './Components/userdetails/UserDetails'
+import User from './Components/users/User'
+import About from "./Components/about/About"
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {
+        path:'/login',
+        element:<Login/>
+      },
+      {
+        path:'/userDetails',
+        element:<UserDetails/>
+      },
+      {
+        path:'/user',
+        element:<User/>
+      },
+      {
+        path:'/about',
+        element:<About/>
+      }
+    ]
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+ <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
